5711feff58c4a931684071ef42805cb354dc71b9
hhhh177 <cwj324013@gmail.com>
2024-03-21 20:41:05 +0800

    拷贝

---
+++ codespace/leetcode/125.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase()
+    let l=0 , r=s.length-1
+    while(l<r){
+    } 
+    function panduan(i,j){
+        if(s[i]){}
+    }
+};
