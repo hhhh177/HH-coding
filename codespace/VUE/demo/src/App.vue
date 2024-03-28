91323e07800019da2aa5a69f5ebd3266f9a74b55
hhhh177 <cwj324013@gmail.com>
2024-03-28 17:11:12 +0800

    main

---
+++ codespace/VUE/demo/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div  class="todo">
+    <input type="text" v-model="content">
+    <button @click="add">添加</button>
+    
+    <List :lists="todos"></List>
+
+  </div>
+
+
+</template>
+
+<script>
+import List from './components/List.vue'
+  export default {
+    components:{
+      List: List
+    },
+    data(){
+      return{
+        conttent:'',
+        todos:['html', 'css' , 'js']
+      }
+    },
+    methods:{
+      add(){
+        console.log(this.content);
+        this.todos.push(this.content);
+      }
+    }
+  }
+</script>
+
+<style lang="css" >
+*{
+  margin: 0;
+  padding: 0;
+}
+#app{
+  text-align: center;
+  font-size: 30px;
+}
+.todo{
+  width: 300px;
+  margin: 0 auto;
+}
+input{
+  padding: 5px 10px;
+}
+button{
+  padding: 5px 10px;
+}
+li{
+  list-style: none;
+  background-color: aliceblue;
+  margin-top: 10px;
+}
+</style>
---
+++ codespace/VUE/learn-vue-router/src/views/Hot.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>沸点页面</h2>
+
+        <button @click="goClasspage">去课程页面</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        methods:{
+            goClasspage(){
+                // this.$router.push('/class')
+                // this.$router.push({path:'/class',query:{id:123}})
+                this.$router.push({name:'class' , params:{id: 456 , name: 'Tom'}})
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
